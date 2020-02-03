309f570591b8e38d6dd4d6f329ae70cfca2a7eed
wusiyu <stephenwu2019@163.com>
2020-02-03 20:43:55 +0800

    parse anwser

--- ch5/simple_lookup.go
+++ ch5/simple_lookup.go
@@ -1,6 +1,8 @@
 package ch5
 
 import (
+	"fmt"
+
 	"github.com/miekg/dns"
 )
 
@@ -8,5 +10,17 @@ func SimpleLookup() {
 	var msg dns.Msg
 	fqdn := dns.Fqdn("baidu.com")
 	msg.SetQuestion(fqdn, dns.TypeA)
-	dns.Exchange(&msg, "8.8.8.8:53")
+	backMsg, err := dns.Exchange(&msg, "8.8.8.8:53")
+	if err != nil {
+		panic(err)
+	}
+	if len(backMsg.Answer) == 0 {
+		fmt.Println("No records")
+		return
+	}
+	for _, answer := range backMsg.Answer {
+		if a, ok := answer.(*dns.A); ok {
+			fmt.Println(a.A)
+		}
+	}
 }
